6cd9990e319419303e05ec57a4f1d17a6c71a88f
Khoi Nguyen <127455746+AwayFromKeyB@users.noreply.github.com>
2026-09-15 15:10:27 -0400

    initial api because of Django convention, contains global configs!

---
+++ bankapi/settings.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "django-insecure-local-dev-key-for-the-no-db-banking-api"
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+
+INSTALLED_APPS = [
+    "rest_framework",
+    "banking",
+]
+
+
+MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+]
+
+
+ROOT_URLCONF = "bankapi.urls"
+WSGI_APPLICATION = "bankapi.wsgi.application"
+TEMPLATES = []
+
+
+DATABASES = {}
+
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+
+REST_FRAMEWORK = {
+
+    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
+
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+
+    "UNAUTHENTICATED_USER": None,
+
+    "EXCEPTION_HANDLER": "banking.errors.api_exception_handler",
+
+    "COERCE_DECIMAL_TO_STRING": False,
+}
